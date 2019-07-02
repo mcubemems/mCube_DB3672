f2ba1e2da15081b2cb6d3b59c0d4fc8d5601e45b
Kevin Huang <binbrave@gmail.com>
2019-07-02 16:47:17 +0800

    Update mCube_shake_hooks.c

--- m_lib_shake/mCube_shake_hooks.c
+++ m_lib_shake/mCube_shake_hooks.c
@@ -1,6 +1,6 @@
 
 #include "mCube_shake_hooks.h"
-#include "m_drv_console.h"
+//#include "m_drv_console.h"
 #include "board_main.h"
 
 /**
@@ -13,6 +13,6 @@
  */
 void mcube_shake_onstatechange(bool newState)
 {
-    am_hal_gpio_out_bit_replace(PIN_LED_R_CTRL, 0x1); // Ambiq Apollo 2 gpio setting
+    am_hal_gpio_out_bit_replace(PIN_LED_R_CTRL, 0x1); // HW gpio setting to light LEDs. You should link to your HW LED pinout
     sprintf(str_l2, "    Shaken");
 }
